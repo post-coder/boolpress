1703cfc250a0ca8a164c654ecf440d29b89f7d3f
post-coder <gabrielspanu.dev@gmail.com>
2024-04-18 10:50:43 +0200

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container py-5">
+
+    <h1>Pagina di amministrazione</h1>
+
+</div>
+
+@endsection
---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container py-5">
+
+    <h1>Tutti gli utenti admin</h1>
+
+</div>
+
+@endsection
