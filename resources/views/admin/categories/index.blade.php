7268c7a541af1042f0c7df0eceb0b0582e1c32c8
post-coder <gabrielspanu.dev@gmail.com>
2024-04-23 12:34:11 +0200

    feat: create and update post with category

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container py-5">
+
+    <h1>Tutte le categorie</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+
+            
+            {{-- @dump($posts) --}}
+            {{-- @foreach($posts as $post)
+            <tr>
+                <th scope="row">{{$loop->index + 1}}</th>
+                <td>{{$post->title}}</td>
+                <td>{{$post->content}}</td>
+                <td><a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-info">Mostra</a></td>
+                <td><a href="{{route('admin.posts.edit', $post->id)}}" class="btn btn-warning">Modifica</a></td>
+            </tr>
+            @endforeach --}}
+
+        </tbody>
+      </table>
+
+</div>
+
+@endsection
