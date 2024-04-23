a15d57337b8eb1bd97fdffafddec1bd47df00600
post-coder <gabrielspanu.dev@gmail.com>
2024-04-23 10:03:33 +0200

    feat: added categories to db and Code

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Web Design',
+            'Web Development',
+            'User Experience',
+            'Private Life'
+        ];
+
+
+        foreach($categories as $category) {
+
+            $newCategory = new Category();
+
+            $newCategory->title = $category;
+
+            $newCategory->save();
+
+        }
+    }
+}
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,6 +9,7 @@
     <h2>Benvenuto {{$user->name}}</h2>
     {{-- @dump($user) --}}
 
+    <a href="{{route('admin.posts.index')}}" class="btn btn-outline-info">Visualizza tutti i post</a>
 
     <a href="{{route('admin.posts.create')}}" class="btn btn-info">Aggiungi un post</a>
 
